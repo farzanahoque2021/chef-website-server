c107831eda317b8208b8688ea4f5457e709d9882
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-05-02 22:26:27 +0600

    Added some data to the chef.json file

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const chef = require('./data/chef.json');
+
 app.get('/', (req, res) => {
     res.send('Server is running')
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
