3a8f1c99b621467310d3b6ba0f958b4ee20a598d
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-05-03 14:56:48 +0600

    Deployed Server in Vercel

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const chef = require('./data/chef.json');
+const category = require('./data/category.json');
 
 app.get('/', (req, res) => {
     res.send('Server is running')
@@ -13,6 +14,22 @@ app.get('/', (req, res) => {
 
 app.get('/chef', (req, res) => {
     res.send(chef);
+})
+
+app.get('/category', (req, res) => {
+    res.send(category);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedChef = chef.find(chef => parseInt(chef.id) === id);
+    res.send(selectedChef);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedCategory = chef.filter(chef => parseInt(chef.category_id) === id);
+    res.send(selectedCategory);
 })
 
 app.listen(port, () => {
